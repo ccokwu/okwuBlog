16f368f640f1be28d69a137fb0dc1e3aac6d90eb
Charlie G <ccokwu@yahoo.com>
2016-07-13 23:40:00 -0400

    updated

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 
 $(document).ready(function() {
   $("#nav").hide()
-    .slideDown(1000)
-    .delay(5000)
-    .slideUp(2000)
+    .slideDown(2000)
+   .delay(5000)
+    // .slideUp(2000)
     .mouseenter(function() {
       $(this).css("display", "inline")
-    });
-  $(".toggle").mouseenter(function() {
-    $("#nav").slideDown(1000).delay(5000);
-  }).mouseleave(function() {
-    $("#nav").slideUp(2000);
-  })
+     });
+  // $(".toggle").mouseenter(function() {
+  //   $("#nav").slideDown(1000).delay(5000);
+  // }).mouseleave(function() {
+  //   $("#nav").slideUp(2000);
+  // })
 
-})
-
-
-$(document).ready(function()) {
   $("#map").googleMap();
   $("#map").addMarker({
     zoom: 10,
     coords: [48.895651, 2.290569],
     url:"index.html",
     type: "ROADMAP"
-  });
-})
+  })
+});
